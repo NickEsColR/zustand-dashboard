028a4a08a89e30dd4c75b88162289a0c26ce2a20
NickEsColR <nicoesol0818@gmail.com>
2024-06-04 07:33:28 -0500

    add persist middleware to save state in storage

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,19 +1,26 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface PersonState {
-  firstName: string;
-  lastName: string;
+    firstName: string;
+    lastName: string;
 }
 
 interface Actions {
-  setFirstName: (firstName: string) => void;
-  setLastName: (lastName: string) => void;
+    setFirstName: (firstName: string) => void;
+    setLastName: (lastName: string) => void;
 }
 
-export const usePersonStore = create<PersonState & Actions>((set) => ({
-  firstName: '',
-  lastName: '',
-
-  setFirstName: (firstName:string) => set({ firstName }),
-  setLastName: (lastName:string) => set({ lastName }),
-}));
+export const usePersonStore = create<PersonState & Actions>()(
+    persist(
+        (set) => ({
+            firstName: "",
+            lastName: "",
+            setFirstName: (firstName: string) => set({ firstName }),
+            setLastName: (lastName: string) => set({ lastName }),
+        }),
+        {
+            name: "person-storage",
+        }
+    )
+);
