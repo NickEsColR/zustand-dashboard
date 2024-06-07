20f33bf31c98f1f3bcb44de823ac276557a30854
NickEsColR <nicoesol0818@gmail.com>
2024-06-07 08:14:19 -0500

    add guest slice

---
+++ src/stores/wedding/guest.slice.ts
@@ -0,0 +1,19 @@
+import { StateCreator } from "zustand";
+
+export interface GuestSlice {
+    guestCount: number;
+
+    // Action creators
+    setGuestCount: (guestCount: number) => void;
+}
+
+export const createGuestSlice: StateCreator<GuestSlice> = (set) => ({
+    guestCount: 0,
+
+    setGuestCount: (guestCount) => {
+        if (guestCount < 0) {
+            return;
+        }
+        set({ guestCount });
+    },
+});
--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -1,12 +1,14 @@
 import { create } from "zustand";
 import { PersonSlice, createPersonSlice } from "./person.slice";
 import { devtools } from "zustand/middleware";
+import { GuestSlice, createGuestSlice } from "./guest.slice";
 
 //?Aqui esta el store
-type ShareState = PersonSlice;
+type ShareState = PersonSlice & GuestSlice;
 
 export const useWeddingBoundStore = create<ShareState>()(
     devtools((...a) => ({
         ...createPersonSlice(...a),
+        ...createGuestSlice(...a),
     }))
 );
